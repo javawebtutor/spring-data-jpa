c82bb9c47d8cb44ba46b39f32b4dc2f919e3e646
mukesh <mrajnitrogmail.com>
2018-02-11 13:48:36 +0530

    Added test cases for get and update scenario

--- one-to-many-1/src/test/java/com/jwt/onetomany/OneToMany1ApplicationTests.java
+++ one-to-many-1/src/test/java/com/jwt/onetomany/OneToMany1ApplicationTests.java
@@ -1,16 +1,18 @@
 package com.jwt.onetomany;
 
-import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jwt.onetomany.entity.Customer;
 import com.jwt.onetomany.entity.PhoneNumber;
 import com.jwt.onetomany.repo.CustomerRepository;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -37,5 +39,28 @@ public class OneToMany1ApplicationTests {
 
 		repository.save(customer);
 	}
+	
+	@Test
+	@Transactional
+	public void testLoadCustomer() {
+		Customer customer = repository.findOne(4L);
+		System.out.println(customer.getName());
+		
+		Set<PhoneNumber> phoneNumbers = customer.getPhoneNumbers();
+		phoneNumbers.forEach(number -> System.out.println(number.getNumber()));
+	}
+	
+	@Test
+	public void testUpdateCustomer() {
+		Customer customer = repository.findOne(4L);
+		customer.setName("Ravi");
+		
+		Set<PhoneNumber> phoneNumbers = customer.getPhoneNumbers();
+		phoneNumbers.forEach(number -> number.setNumber("893965"));
+		
+		repository.save(customer);
+	}
+	
+	
 
 }
