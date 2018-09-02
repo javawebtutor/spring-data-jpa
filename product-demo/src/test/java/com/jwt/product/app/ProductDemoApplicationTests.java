c2c2e35cbf1df43ea0caa6e922aa71472d24d9ae
javawebtutor <mrajnitro@gmail.com>
2018-09-02 15:15:20 +0530

    Adding test case for saving produt

--- product-demo/src/test/java/com/jwt/product/app/ProductDemoApplicationTests.java
+++ product-demo/src/test/java/com/jwt/product/app/ProductDemoApplicationTests.java
@@ -2,15 +2,34 @@ package com.jwt.product.app;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.jwt.product.app.entity.Product;
+import com.jwt.product.app.repo.ProductRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProductDemoApplicationTests {
 
+	@Autowired
+	ProductRepository productRepository;
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testCreate() {
+
+		Product product = new Product();
+		product.setId(1);
+		product.setName("Iphone");
+		product.setDesc("Awesome");
+		product.setPrice(1000d);
+		productRepository.save(product);
+
 	}
 
 }
